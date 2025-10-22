bb36e61e502b56b8de7dc1cead46669521c2fa61
Michael Harhay <Michael_Harhay@arxtron.com>
2025-10-22 11:23:43 -0400

    Summary title for release * Point1 * Point2 * Point3

--- cvibuild.TestLib/Release/postBuild.bat
+++ cvibuild.TestLib/Release/postBuild.bat
@@ -1,20 +1,20 @@
 @set CVIDIR=c:\program files (x86)\national instruments\cvi2019
 @set CVISHAREDDIR=C:\Program Files (x86)\National Instruments\Shared\CVI
 @set CVIPUBDOCSDIR=C:\Users\Public\Documents\National Instruments\CVI
-@set CVIPROJPATH=c:\Arxtron\RD25XXX_CICD\Source\TestLib.prj
-@set CVIPROJDIR=c:\Arxtron\RD25XXX_CICD\Source
+@set CVIPROJPATH=c:\Users\Michael_Harhay\Desktop\TestLib\TestLib.prj
+@set CVIPROJDIR=c:\Users\Michael_Harhay\Desktop\TestLib
 @set CVIPROJNAME=TestLib.prj
-@set CVITARGETPATH=c:\Arxtron\RD25XXX_CICD\Source\TestLib.dll
-@set CVITARGETDIR=c:\Arxtron\RD25XXX_CICD\Source
+@set CVITARGETPATH=c:\Users\Michael_Harhay\Desktop\TestLib\TestLib.dll
+@set CVITARGETDIR=c:\Users\Michael_Harhay\Desktop\TestLib
 @set CVITARGETNAME=TestLib.dll
 @set CVIBUILDCONFIG=Release
-@set CVITARGETFILEVER=2.0.0.3
+@set CVITARGETFILEVER=1.1.0.0
 @set CVITARGETPRODVER=0.0.0.0
 @set CVIVXIPNPDIR=C:\Program Files (x86)\IVI Foundation\VISA\winnt
 @set CVIIVIDIR=C:\Program Files (x86)\IVI Foundation\IVI
 @set CVIWINDIR=C:\WINDOWS
 @set CVISYSDIR=C:\WINDOWS\system32
-@pushd "c:\Arxtron\RD25XXX_CICD\Source"
+@pushd "c:\Users\Michael_Harhay\Desktop\TestLib"
 @setlocal
 @set _some_build_step_failed=0
 copy "%CVITARGETDIR%\*.dll" "C:\Arxtron\Libraries\"
